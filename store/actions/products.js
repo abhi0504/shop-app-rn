54a11f34ab4b744b8b33c3a1b70cc9e6e159e72e
Abhishek Sharma <sharmaash876@gmail.com>
2020-09-09 22:18:49 +0530

    setting up firebase as backend

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,29 +1,56 @@
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
-export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+import Product from '../../models/product'
+
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
 };
 
-export const fetchProducts = products => {
-  const response = await fetch('https://shop-rn-f2ff2.firebaseio.com/products.json' , {
-    body : JSON.stringify({
-      title,
-      description,
-      imageUrl,
-      price
-    })
-  })
+export const fetchProducts = () => {
+  return async dispatch => {
+    // any async code you want!
+    try {
+      const response = await fetch(
+        'https://rn-complete-guide-87900.firebaseio.com/product.json'    );
 
-}
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            'u1',
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // send to custom analytics server
+      throw err;
+    }
+  };
+};
+
 
 export const createProduct = (title, description, imageUrl, price) => {
 
   return async dispatch => {
     
-   const response = await fetch('https://shop-rn-f2ff2.firebaseio.com/products.json' , {
+   const response = await fetch('https://rn-complete-guide-87900.firebaseio.com/products.json' , {
       method : 'POST',
       headers : {
         'Content-Type' : 'application/json'
@@ -64,3 +91,6 @@ export const updateProduct = (id, title, description, imageUrl) => {
     }
   };
 };
+
+
+//https://rn-complete-guide-87900.firebaseio.com/products.json
